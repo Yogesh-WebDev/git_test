b0e2b49a40dfe3ea953490ab9e955b91f0499fdb
Yogesh-WebDev <yogesh.raut@indexnine.com>
2021-06-21 16:27:19 +0530

    final Etch-A-Sketch

--- Etch-A-Sketch/script.js
+++ Etch-A-Sketch/script.js
@@ -1,20 +1,47 @@
-const display=document.querySelector('#container');
-display.addEventListener("mouseover",function (e) {
-    e.target.style.color = "white";
-})
-const divOver=document.createElement('div');
-divOver.classList.add('square');
+const display = document.querySelector('#container');
+let input;
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', function () {
+    input = prompt("Enter grid size");
 
-for (let indexRow = 1; indexRow < 17; indexRow++) {
-    for (let indexColumn = 1; indexColumn < 17; indexColumn++) {
-        divOver.style.cssText=`
-                grid-column-start:${indexColumn} ;
-                grid-column-end: ${indexColumn++};
-                grid-row-start: ${indexRow};
-                grid-row-end: ${indexRow++};
+    generateGride(Number(input));
+});
+function generateGride(input) {
+
+    display.style.cssText = `
+        display: grid;
+        grid-template-columns: repeat(${input}, 1fr);
+        grid-template-rows: repeat(${input}, 1fr);
+        grid-auto-flow: dense;
+        grid-auto-rows: 1fr;
+         background-color: black;
+         height: 960px;
+        width: 960px;
         `;
-        display.appendChild(divOver);
-    }
-    
-}
 
+    for (let indexRow = 1; indexRow < input + 1; indexRow++) {
+        for (let indexColumn = 1; indexColumn < input + 1; indexColumn++) {
+            const divOver = document.createElement('div');
+            divOver.classList.add('square');
+            divOver.style.cssText = `
+                grid-column-start:${indexColumn} ;
+                grid-column-end: ${indexColumn + 1};
+                grid-row-start: ${indexRow};
+                grid-row-end: ${indexRow + 1};
+                
+        `;
+            display.appendChild(divOver);
+        }
+    }
+    addListner();
+}
+function addListner() {
+    const squares = document.querySelectorAll('.square');
+
+    squares.forEach(square => {
+        square.addEventListener("mouseenter", function (e) {
+            e.target.style.backgroundColor = 'white';
+            //console.log(e.target);
+        })
+    });
+}
